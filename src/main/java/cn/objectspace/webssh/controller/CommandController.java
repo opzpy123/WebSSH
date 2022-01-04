164f270bc3bb2fcf56e9776247a6bf41ed09ed57
opzpy <1441120973@qq.com>
2022-01-04 17:03:26 +0800

    命令行 exec

--- src/main/java/cn/objectspace/webssh/controller/CommandController.java
+++ src/main/java/cn/objectspace/webssh/controller/CommandController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("cmd")
@@ -19,9 +20,15 @@ public class CommandController {
             Process pro = Runtime.getRuntime().exec(cmd.getCmd());
             pro.waitFor();
             in = pro.getInputStream();
-            BufferedReader read = new BufferedReader(new InputStreamReader(in));
-            System.out.println(read.readLine());
-            return read.readLine();
+            BufferedReader read = new BufferedReader(new InputStreamReader(in, "GBK"));
+            StringBuilder sb = new StringBuilder();
+            sb.append("命令执行结果:\n");
+            while (read.ready()) {
+                String s = read.readLine();
+                sb.append(s);
+                System.out.println(s);
+            }
+            return sb.toString();
         } catch (Exception e) {
             return e.getMessage();
         }
